220f15829ced79a3976d36714da89ee104f7e951
Oscar Maia <oscarfgmaia@gmail.com>
2023-09-05 21:46:18 -0300

    chore: add fake database

---
+++ frontend/constants/index.ts
@@ -0,0 +1,47 @@
+export const Products = [
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/006a/8653/3fd45771ef0ab8e8b4230c22de407513?Expires=1694995200&Signature=BEn3fwf5xlWUjqwSVKlhygdvddiaIjo6sdMqYkp2xG-Zp55hyjKgtozfwNufKh98r8cU~7hd5LyWa6trYJCwwyzBzIkhRxnm2T4-fm~UQiu47ILnO10bpWDbqb--tF58XT~uPK-MV9IjTjZC6EzRYxLfurKRczlPlOTwiulXL3GYL6dy5hzyeTw9cTBEWL99XBmNeKePsigu58MfZYFQAk2z0ud88EeTccRVxPppU-BzFbqZLJ6zJIHWbWBaKppnkHjH8mF1dm0JgfdQJlgqsx~kSl3m0DrRW7d~ansMzAeG3IG-6oE0BW6Bx2Qb5AQ~q94lN87lGIrcLKh84zjh-Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    name: "Tênis marrom",
+    description:
+      "Sapatenis Masculino Branco Tênis Casual FLOW para Dia a Dia - Camurça",
+    price: 28000,
+    id: 0,
+    secundary_images: [
+      "https://s3-alpha-sig.figma.com/img/006a/8653/3fd45771ef0ab8e8b4230c22de407513?Expires=1694995200&Signature=BEn3fwf5xlWUjqwSVKlhygdvddiaIjo6sdMqYkp2xG-Zp55hyjKgtozfwNufKh98r8cU~7hd5LyWa6trYJCwwyzBzIkhRxnm2T4-fm~UQiu47ILnO10bpWDbqb--tF58XT~uPK-MV9IjTjZC6EzRYxLfurKRczlPlOTwiulXL3GYL6dy5hzyeTw9cTBEWL99XBmNeKePsigu58MfZYFQAk2z0ud88EeTccRVxPppU-BzFbqZLJ6zJIHWbWBaKppnkHjH8mF1dm0JgfdQJlgqsx~kSl3m0DrRW7d~ansMzAeG3IG-6oE0BW6Bx2Qb5AQ~q94lN87lGIrcLKh84zjh-Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/006a/8653/3fd45771ef0ab8e8b4230c22de407513?Expires=1694995200&Signature=BEn3fwf5xlWUjqwSVKlhygdvddiaIjo6sdMqYkp2xG-Zp55hyjKgtozfwNufKh98r8cU~7hd5LyWa6trYJCwwyzBzIkhRxnm2T4-fm~UQiu47ILnO10bpWDbqb--tF58XT~uPK-MV9IjTjZC6EzRYxLfurKRczlPlOTwiulXL3GYL6dy5hzyeTw9cTBEWL99XBmNeKePsigu58MfZYFQAk2z0ud88EeTccRVxPppU-BzFbqZLJ6zJIHWbWBaKppnkHjH8mF1dm0JgfdQJlgqsx~kSl3m0DrRW7d~ansMzAeG3IG-6oE0BW6Bx2Qb5AQ~q94lN87lGIrcLKh84zjh-Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/006a/8653/3fd45771ef0ab8e8b4230c22de407513?Expires=1694995200&Signature=BEn3fwf5xlWUjqwSVKlhygdvddiaIjo6sdMqYkp2xG-Zp55hyjKgtozfwNufKh98r8cU~7hd5LyWa6trYJCwwyzBzIkhRxnm2T4-fm~UQiu47ILnO10bpWDbqb--tF58XT~uPK-MV9IjTjZC6EzRYxLfurKRczlPlOTwiulXL3GYL6dy5hzyeTw9cTBEWL99XBmNeKePsigu58MfZYFQAk2z0ud88EeTccRVxPppU-BzFbqZLJ6zJIHWbWBaKppnkHjH8mF1dm0JgfdQJlgqsx~kSl3m0DrRW7d~ansMzAeG3IG-6oE0BW6Bx2Qb5AQ~q94lN87lGIrcLKh84zjh-Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/006a/8653/3fd45771ef0ab8e8b4230c22de407513?Expires=1694995200&Signature=BEn3fwf5xlWUjqwSVKlhygdvddiaIjo6sdMqYkp2xG-Zp55hyjKgtozfwNufKh98r8cU~7hd5LyWa6trYJCwwyzBzIkhRxnm2T4-fm~UQiu47ILnO10bpWDbqb--tF58XT~uPK-MV9IjTjZC6EzRYxLfurKRczlPlOTwiulXL3GYL6dy5hzyeTw9cTBEWL99XBmNeKePsigu58MfZYFQAk2z0ud88EeTccRVxPppU-BzFbqZLJ6zJIHWbWBaKppnkHjH8mF1dm0JgfdQJlgqsx~kSl3m0DrRW7d~ansMzAeG3IG-6oE0BW6Bx2Qb5AQ~q94lN87lGIrcLKh84zjh-Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    ],
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/e8b8/af2d/17c51bb4f41f2fa39974cef4d87b1158?Expires=1694390400&Signature=likl-biPy3L8Vfjlo0GhB6PL4iQOCaCbHq~btELhg9faEqYR5v3ZpjtD8qDmwx2Vg9OlYS-sWz2AsbeouYTxvD~F0jh3B~wRYKSafa6frRTYYVtVUXuCmrxx0p-I~zvtGCDBMQOLE7ucBvTmwC~4kU145FaVM2LcDiWFcIuQ-zyx8ujP3JvNRmilbB~itWN5ZHWXtg-dK9ViPb3Um72jIRWjmcC4dZ0dDOzcg0ST~zBkUSKLszycYE3iSRLxmfJoVmFFdoUzj0aUYF4Cy~yw~~I6r3P45c0VJUjwzTLNTeyQj~SJF5NtNYRS8UUt7kSVmoPxcrEa77coesPLE~e6KA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    name: "Tênis branco",
+    price: 30000,
+    description:
+      "Sapatenis Masculino Branco Tênis Casual FLOW para Dia a Dia - Camurça",
+    id: 1,
+    secundary_images: [
+      "https://s3-alpha-sig.figma.com/img/e8b8/af2d/17c51bb4f41f2fa39974cef4d87b1158?Expires=1694390400&Signature=likl-biPy3L8Vfjlo0GhB6PL4iQOCaCbHq~btELhg9faEqYR5v3ZpjtD8qDmwx2Vg9OlYS-sWz2AsbeouYTxvD~F0jh3B~wRYKSafa6frRTYYVtVUXuCmrxx0p-I~zvtGCDBMQOLE7ucBvTmwC~4kU145FaVM2LcDiWFcIuQ-zyx8ujP3JvNRmilbB~itWN5ZHWXtg-dK9ViPb3Um72jIRWjmcC4dZ0dDOzcg0ST~zBkUSKLszycYE3iSRLxmfJoVmFFdoUzj0aUYF4Cy~yw~~I6r3P45c0VJUjwzTLNTeyQj~SJF5NtNYRS8UUt7kSVmoPxcrEa77coesPLE~e6KA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/e8b8/af2d/17c51bb4f41f2fa39974cef4d87b1158?Expires=1694390400&Signature=likl-biPy3L8Vfjlo0GhB6PL4iQOCaCbHq~btELhg9faEqYR5v3ZpjtD8qDmwx2Vg9OlYS-sWz2AsbeouYTxvD~F0jh3B~wRYKSafa6frRTYYVtVUXuCmrxx0p-I~zvtGCDBMQOLE7ucBvTmwC~4kU145FaVM2LcDiWFcIuQ-zyx8ujP3JvNRmilbB~itWN5ZHWXtg-dK9ViPb3Um72jIRWjmcC4dZ0dDOzcg0ST~zBkUSKLszycYE3iSRLxmfJoVmFFdoUzj0aUYF4Cy~yw~~I6r3P45c0VJUjwzTLNTeyQj~SJF5NtNYRS8UUt7kSVmoPxcrEa77coesPLE~e6KA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/e8b8/af2d/17c51bb4f41f2fa39974cef4d87b1158?Expires=1694390400&Signature=likl-biPy3L8Vfjlo0GhB6PL4iQOCaCbHq~btELhg9faEqYR5v3ZpjtD8qDmwx2Vg9OlYS-sWz2AsbeouYTxvD~F0jh3B~wRYKSafa6frRTYYVtVUXuCmrxx0p-I~zvtGCDBMQOLE7ucBvTmwC~4kU145FaVM2LcDiWFcIuQ-zyx8ujP3JvNRmilbB~itWN5ZHWXtg-dK9ViPb3Um72jIRWjmcC4dZ0dDOzcg0ST~zBkUSKLszycYE3iSRLxmfJoVmFFdoUzj0aUYF4Cy~yw~~I6r3P45c0VJUjwzTLNTeyQj~SJF5NtNYRS8UUt7kSVmoPxcrEa77coesPLE~e6KA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/e8b8/af2d/17c51bb4f41f2fa39974cef4d87b1158?Expires=1694390400&Signature=likl-biPy3L8Vfjlo0GhB6PL4iQOCaCbHq~btELhg9faEqYR5v3ZpjtD8qDmwx2Vg9OlYS-sWz2AsbeouYTxvD~F0jh3B~wRYKSafa6frRTYYVtVUXuCmrxx0p-I~zvtGCDBMQOLE7ucBvTmwC~4kU145FaVM2LcDiWFcIuQ-zyx8ujP3JvNRmilbB~itWN5ZHWXtg-dK9ViPb3Um72jIRWjmcC4dZ0dDOzcg0ST~zBkUSKLszycYE3iSRLxmfJoVmFFdoUzj0aUYF4Cy~yw~~I6r3P45c0VJUjwzTLNTeyQj~SJF5NtNYRS8UUt7kSVmoPxcrEa77coesPLE~e6KA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    ],
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/27f5/b7a5/c4575cacc24391e72189abb71f8b9999?Expires=1694995200&Signature=O9GaBfCKuLeSuaMHIXY3ga7JNt8rEp6zPENCOJk0meCZF6nvyGkCu3eT~CaG9OGs0bEYoDocWXMIt4k--0zqvMaVua2OQV5rEnaliYYnSyq68geGhwm0sadJHtKHVjJEgxgRjwEQiTD3~VsxLCZ4sEQ9Oe~cXXBjIhTsADJ~u6uFygsvJAQN1wpyPyfxMyW1rbooOCBgOvezTQZAwu79VNDzJfPZHyeX5SSKpSV2O9ZrcIlfwNBKxO0-bE4tWISuidx7DdRc832aqqfJQ~ozMkgokBvnseqpNnWVgeWhfMkUVUpTNqYsel2IledP2WHFCX2YfFvgIPBA3cQl6EKhUg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    name: "Tênis vermelho",
+    price: 32000,
+    description:
+      "Sapatenis Masculino Branco Tênis Casual FLOW para Dia a Dia - Camurça",
+    id: 2,
+    secundary_images: [
+      "https://s3-alpha-sig.figma.com/img/27f5/b7a5/c4575cacc24391e72189abb71f8b9999?Expires=1694995200&Signature=O9GaBfCKuLeSuaMHIXY3ga7JNt8rEp6zPENCOJk0meCZF6nvyGkCu3eT~CaG9OGs0bEYoDocWXMIt4k--0zqvMaVua2OQV5rEnaliYYnSyq68geGhwm0sadJHtKHVjJEgxgRjwEQiTD3~VsxLCZ4sEQ9Oe~cXXBjIhTsADJ~u6uFygsvJAQN1wpyPyfxMyW1rbooOCBgOvezTQZAwu79VNDzJfPZHyeX5SSKpSV2O9ZrcIlfwNBKxO0-bE4tWISuidx7DdRc832aqqfJQ~ozMkgokBvnseqpNnWVgeWhfMkUVUpTNqYsel2IledP2WHFCX2YfFvgIPBA3cQl6EKhUg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/27f5/b7a5/c4575cacc24391e72189abb71f8b9999?Expires=1694995200&Signature=O9GaBfCKuLeSuaMHIXY3ga7JNt8rEp6zPENCOJk0meCZF6nvyGkCu3eT~CaG9OGs0bEYoDocWXMIt4k--0zqvMaVua2OQV5rEnaliYYnSyq68geGhwm0sadJHtKHVjJEgxgRjwEQiTD3~VsxLCZ4sEQ9Oe~cXXBjIhTsADJ~u6uFygsvJAQN1wpyPyfxMyW1rbooOCBgOvezTQZAwu79VNDzJfPZHyeX5SSKpSV2O9ZrcIlfwNBKxO0-bE4tWISuidx7DdRc832aqqfJQ~ozMkgokBvnseqpNnWVgeWhfMkUVUpTNqYsel2IledP2WHFCX2YfFvgIPBA3cQl6EKhUg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/27f5/b7a5/c4575cacc24391e72189abb71f8b9999?Expires=1694995200&Signature=O9GaBfCKuLeSuaMHIXY3ga7JNt8rEp6zPENCOJk0meCZF6nvyGkCu3eT~CaG9OGs0bEYoDocWXMIt4k--0zqvMaVua2OQV5rEnaliYYnSyq68geGhwm0sadJHtKHVjJEgxgRjwEQiTD3~VsxLCZ4sEQ9Oe~cXXBjIhTsADJ~u6uFygsvJAQN1wpyPyfxMyW1rbooOCBgOvezTQZAwu79VNDzJfPZHyeX5SSKpSV2O9ZrcIlfwNBKxO0-bE4tWISuidx7DdRc832aqqfJQ~ozMkgokBvnseqpNnWVgeWhfMkUVUpTNqYsel2IledP2WHFCX2YfFvgIPBA3cQl6EKhUg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://s3-alpha-sig.figma.com/img/27f5/b7a5/c4575cacc24391e72189abb71f8b9999?Expires=1694995200&Signature=O9GaBfCKuLeSuaMHIXY3ga7JNt8rEp6zPENCOJk0meCZF6nvyGkCu3eT~CaG9OGs0bEYoDocWXMIt4k--0zqvMaVua2OQV5rEnaliYYnSyq68geGhwm0sadJHtKHVjJEgxgRjwEQiTD3~VsxLCZ4sEQ9Oe~cXXBjIhTsADJ~u6uFygsvJAQN1wpyPyfxMyW1rbooOCBgOvezTQZAwu79VNDzJfPZHyeX5SSKpSV2O9ZrcIlfwNBKxO0-bE4tWISuidx7DdRc832aqqfJQ~ozMkgokBvnseqpNnWVgeWhfMkUVUpTNqYsel2IledP2WHFCX2YfFvgIPBA3cQl6EKhUg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    ],
+  },
+];
